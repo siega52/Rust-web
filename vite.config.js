08230b6fc2e7d01dfc2aa551b1a3d47ad1e8782c
siega52 <130769490+siega52@users.noreply.github.com>
2026-01-04 20:56:58 +0300

    update files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'Rust-web',
+  base: '/Rust-web/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
